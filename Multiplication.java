31cdc1fe24a025aa6a2151b37482ea982000121e
Markskoblionok <skoblionokm@mail.ru>
2016-10-04 08:41:00 +0300

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,37 @@
+public class Multiplication{
+  public static void main(String[] args){
+    int[][] a = {
+      {9, 1, 4, 6, 0},
+      {5, -4, 8, -6, 2},
+      {9, -2, 3, -7, 6},
+      {5, -5, 4, -8, 7},
+      {1, -2, 3, -4, 5}
+    };
+    int[][] b = {
+      {1 , 2 , 3 , 4 },
+      {5 , -6, 7, -8 },
+      {9 , 8 , -7 , 6},
+      {5, 4, -3, 2},
+      {9 , 5, -3, 6}
+    };
+    int al = a.length;
+    int bol = b[0].length;
+    int bl = b.length;
+    int[][] c = new int[al][bol];
+    if (a[0].length == b.length){
+      for (int i = 0; i < al; i++) {
+        for (int j = 0; j < bol; j++) {
+          for (int k = 0; k < bl; k++) {
+           c[i][j] += a[i][k] * b[k][j];
+          }
+        }
+      }
+    }
+    for (int i = 0; i < c.length; i++) {
+    for (int j = 0; j < c[0].length; j++) {
+        System.out.println(c[i][j]);
+    }
+    System.out.println();
+}
+  }
+}
